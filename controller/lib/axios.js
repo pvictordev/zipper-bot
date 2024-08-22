cae53d9ab373667bc8106d7e9f06f1acd9f12d45
Victor Purice <victor.purice@ulbsibiu.ro>
2024-08-22 10:26:28 +0300

    wrote js code to handle bot requests

---
+++ controller/lib/axios.js
@@ -0,0 +1,22 @@
+const axios = require("axios");
+
+function getAxiosInstance(BASE_URL, headers = {}) {
+	return {
+		get(method, params) {
+			return axios.get(`/${method}`, {
+				baseURL: BASE_URL,
+			});
+		},
+		post(method, data) {
+			return axios({
+				method: "post",
+				baseURL: BASE_URL,
+				url: `/${method}`,
+				data,
+				headers,
+			});
+		},
+	};
+}
+
+module.exports = { getAxiosInstance };
--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const express = require("express");
 
 const PORT = process.env.port || 4040;
-
+const { handler } = require("./controller");
 const app = express();
 
+// middleware to parse JSON bodies
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.post("*", async (req, res) => {
-	res.send("Hello post");
+	console.log(req.body);
+	res.send(await handler(req, "POST"));
 });
 
 app.get("*", async (req, res) => {
-	res.send("Hello get");
+	res.send(await handler(req, "GET"));
 });
 
 app.listen(PORT, function (err) {

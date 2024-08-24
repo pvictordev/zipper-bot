37d156d9b0507381d425bd9d346c84a15a5f338c
Victor Purice <victor.purice@ulbsibiu.ro>
2024-08-24 16:01:01 +0300

    refactored bot logic to localte all the code in the index.js file

--- index.js
+++ index.js
@@ -1,23 +1,51 @@
+require("dotenv").config();
 const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
 
-const PORT = process.env.port || 4040;
-const { handler } = require("./controller");
+const PORT = process.env.PORT || 4040;
 const app = express();
+
+const { TOKEN, NGROK_URL } = process.env;
+
+const TELEGRAM_API = `https://api.telegram.org/bot${TOKEN}`;
+const URI = `/webhook/${TOKEN}`;
+const WEBHOOK_URL = NGROK_URL + URI;
+
+// provide the webhook to telegram, so the server can know where it sends the updates
+
+const init = async () => {
+	const res = await axios.get(
+		`${TELEGRAM_API}/setWebhook?url=${WEBHOOK_URL}`
+	);
+	console.log(res.data);
+};
 
 // middleware to parse JSON bodies
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.post("*", async (req, res) => {
+app.post(URI, async (req, res) => {
 	console.log(req.body);
-	res.send(await handler(req, "POST"));
+
+	const chatId = req.body.message.chat.id;
+	const message = req.body.message.text;
+
+	// sent a message back
+	await axios.post(`${TELEGRAM_API}/sendMessage`, {
+		chat_id: chatId,
+		text: message,
+	});
+
+	return res.send();
 });
 
 app.get("*", async (req, res) => {
-	res.send(await handler(req, "GET"));
+	res.send("GET request");
 });
 
-app.listen(PORT, function (err) {
+app.listen(PORT, async (err) => {
 	if (err) console.log(err);
-	console.log("Server is listening on PORT", PORT);
+	console.log("🚀 Server is running on PORT", PORT);
+	await init();
 });

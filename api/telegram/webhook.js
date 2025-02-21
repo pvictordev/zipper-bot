6b407fbfed58e47afb94fafca2653e98751d0ffa
pvictor <pvictor0110@gmail.com>
2025-02-21 02:20:01 +0200

    fix: webhook endpoint

--- api/telegram/webhook.js
+++ api/telegram/webhook.js
@@ -1,25 +1,31 @@
+import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 export default async function handler(req, res) {
   if (req.method === "POST") {
     try {
       const { message } = req.body;
+
       if (message) {
         const chatId = message.chat.id;
         const text = message.text || "I can only handle text messages.";
 
-        await fetch(
+        await axios.post(
           `https://api.telegram.org/bot${process.env.TOKEN}/sendMessage`,
           {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ chat_id: chatId, text }),
+            chat_id: chatId,
+            text: text,
           }
         );
       }
+
       return res.status(200).json({ status: "ok" });
     } catch (err) {
       console.error("Error:", err);
+      return res.status(500).json({ error: "Something went wrong." });
     }
-    return res.status(500).json({ error: "Something went wrong." });
   } else {
     res.status(200).send("Telegram Webhook Endpoint");
   }
